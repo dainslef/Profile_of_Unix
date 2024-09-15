72cc09b2265a3e23f8e227acd387a90f471b2652
dainslef <dainslef@outlook.com>
2024-09-15 23:52:10 +0800

    Update configuration deploy script.

--- tools/configurations_setup.fish
+++ tools/configurations_setup.fish
@@ -1,12 +1,15 @@
 # Get base repo path by plantform.
 if [ (uname) = Linux ]
     set HOME_PATH /home/dainslef
+    set KITTY_CONFIG kitty.conf
 else if [ (uname) = Darwin ]
     set HOME_PATH /Users/dainslef
+    set KITTY_CONFIG kitty-macos.conf
 end
 
 # Set the repo base path.
-set REPO_PATH $HOME_PATH/Public/Syncthing/Work/Codes/GitRepo/ProfileOfUnix
+set REPO_PATH (realpath ../)
+echo "Current config repo path is $REPO_PATH."
 
 # Git configurations
 echo -n "Set up git config ... "
@@ -22,7 +25,7 @@ echo OK
 
 # VIM
 echo -n "Set up VIM ... "
-if ! [ -e ~/.vim/bundle/Vundle.vim ]
+if not [ -e ~/.vim/bundle/Vundle.vim ]
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 end
 ln -sf $REPO_PATH/.vimrc ~/.vimrc
@@ -31,7 +34,7 @@ echo OK
 # Ansible
 echo -n "Set up Ansible ... "
 mkdir ~/.ansible
-if ! [ -e ~/.ansible/hosts ]
+if not [ -e ~/.ansible/hosts ]
     cp $REPO_PATH/ansible/hosts ~/.ansible/hosts
 end
 ln -sf $REPO_PATH/ansible/ansible.cfg ~/.ansible.cfg
@@ -59,7 +62,7 @@ if type -q awesome
 end
 
 # Link the theme configuration manually only if use Window Manager
-if type -q qtile || type -q awesome
+if type -q qtile or type -q awesome
     # GTK theme
     echo -ne "Set up GTK theme ... "
     mkdir -p ~/.config/gtk-3.0
@@ -87,26 +90,15 @@ end
 if type -q kitty
     echo -n "Set up Kitty ... "
     mkdir -p ~/.config/kitty
-    ln -sf $REPO_PATH/shell/kitty.conf ~/.config/kitty/kitty.conf
+    ln -sf $REPO_PATH/shell/$KITTY_CONFIG ~/.config/kitty/kitty.conf
     echo OK
 end
 
-if type -q btop
-    echo -n "Set up Btop ... "
-    mkdir -p ~/.config/btop
-    ln -sf $REPO_PATH/btop.conf ~/.config/btop/btop.conf
-    # Btop process will try to overwrite config, so lock file write permisson.
-    chmod 400 ~/.config/btop/btop.conf
-    echo OK
-end
-
-# Oh ny fish
+# Oh ny fish, need to install OMF and theme at first:
+#
+# curl -L https://get.oh-my.fish | fish
+# omf install bobthefish
 echo -ne "Set up Fish shell ... "
-if ! [ -e ~/.config/omf ] # Check if OMF has areadly downloaded.
-    curl -L https://get.oh-my.fish | nohup fish >/dev/null
-    source .local/share/omf/init.fish
-    omf install bobthefish
-end
 ln -sf $REPO_PATH/shell/config.fish ~/.config/fish/config.fish
 echo OK
 
@@ -118,20 +110,5 @@ if [ (uname) = Linux ]
         echo "Current OS is Arch Linux, set up addition configuration ..."
         mkdir -p ~/.config/fontconfig
         ln -sf $REPO_PATH/xorg/fonts.xml ~/.config/fontconfig/fonts.conf # Fonts configurations.
-
-        # Create systemd user service dir if not exist.
-        mkdir -p ~/.config/systemd/user/
-
-        ln -sf $REPO_PATH/systemd/fcitx5.service ~/.config/systemd/user/fcitx5.service
-        systemctl --user enable fcitx5
-        systemctl --user start fcitx5
-
-        ln -sf $REPO_PATH/systemd/nm-applet.service ~/.config/systemd/user/nm-applet.service
-        systemctl --user enable nm-applet
-        systemctl --user start nm-applet
-
-        ln -sf $REPO_PATH/systemd/picom.service ~/.config/systemd/user/picom.service
-        systemctl --user enable picom
-        systemctl --user start picom
     end
 end
